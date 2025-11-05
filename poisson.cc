fc5ad590575910b54a60f4e9d913ddd6413ad0ad
Mahtab <mahtab.jalal.vandi@cern.ch>
2025-11-05 16:33:19 +0100

    write hist.txt + visualise

--- poisson.cc
+++ poisson.cc
@@ -4,26 +4,38 @@
 using namespace std;
 
 int main() {
-    vector<int> zaehler(11, 0); 
+
+    vector<int> zaehler(11, 0);
+
 
     ifstream fin("datensumme.txt");
-    if (!fin.is_open()) {
+    if (!fin) {
         cerr << "Error: could not open datensumme.txt" << endl;
         return 1;
     }
-
     int zahl;
     while (fin >> zahl) {
-        if (zahl >= 0 && zahl <= 10) {
+        if (0 <= zahl && zahl <= 10) {
             zaehler[zahl] += 1;
         }
     }
-
     fin.close();
+
 
     for (unsigned int k = 0; k < zaehler.size(); ++k) {
         cout << k << " : " << zaehler[k] << endl;
     }
 
+    ofstream fout("hist.txt");
+    if (!fout) {
+        cerr << "Error: could not write hist.txt" << endl;
+        return 1;
+    }
+    for (int k = 0; k <= 10; ++k) {
+        fout << k << " " << zaehler[k] << "\n";
+    }
+    fout.close();
+
+    cout << "Wrote counts to hist.txt" << endl;
     return 0;
 }
