718c03d0bc746f1a4002d7356c5dffcca3a2e9ab
jialiang <jialiang.sun@cern.ch>
2025-11-19 13:47:44 +0100

    Update exercise 3

--- poisson.cc
+++ poisson.cc
@@ -1,20 +1,29 @@
-#include <iostream>
+#include <cmath>
 #include <fstream>
+#include <iostream>
 #include <vector>
 
-
-double poisson(double mu, int k) {
-    return 0;
+double poisson(double mu, int k)
+{
+  return pow(mu, k) * exp(-mu) / tgamma(k + 1);
 }
 
-int main() {
-    using namespace std;
-
-
-    ifstream fin("datensumme.txt");
-    int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-    }
-    fin.close();
+int main()
+{
+  using namespace std;
+  vector<int> counter(11);
+  ifstream fin("datensumme.txt");
+  ofstream hist("hist.txt"), histpoi("histpoi.txt");
+  int goal;
+  while (fin >> goal)
+  {
+    counter[goal] += 1;
+  }
+  for (unsigned int k = 0; k < counter.size(); ++k)
+  {
+    cout << k << ":" << counter[k] << endl;
+    hist << k << "\t" << counter[k] << endl;
+    histpoi << k << "\t" << counter[k] << "\t" << 234 * poisson(3.11538, k)
+            << endl;
+  }
 }
